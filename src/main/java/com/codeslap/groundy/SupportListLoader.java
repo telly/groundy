6ee211bed5477f7d87547383eec9c4ce7877d355
Cristian <cristian@elhacker.net>
2012-12-01 17:44:29 -0800

    Separated implementations

--- src/main/java/com/codeslap/groundy/SupportListLoader.java
+++ src/main/java/com/codeslap/groundy/SupportListLoader.java
@@ -1,18 +1,34 @@
+/*
+ * Copyright 2012 CodeSlap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.codeslap.groundy;
 
-import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
 
 import java.util.List;
 
 /**
- * @author cristian
- * @version 1.0
+ * AsyncTaskLoader which handles lists of genereic time
+ * @param <T>
  */
-public abstract class ListLoader<T> extends AsyncTaskLoader<List<T>> {
+public abstract class SupportListLoader<T> extends AsyncTaskLoader<List<T>> {
     private List<T> mList;
 
-    public ListLoader(Context context) {
+    public SupportListLoader(Context context) {
         super(context);
     }
 
